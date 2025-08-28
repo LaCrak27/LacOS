f72779a7e06316d902a2aefd1e6c5ffc5e4aafc8
LaCrak27 <ivancrak123@gmail.com>
2025-08-28 14:10:35 +0200

    Removed garbage file, also re-set stack in kernel entry

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,6 +1,8 @@
 ; Jumps to the kernel's entry function
 [bits 32]
 extern kmain
-xor ebp, ebp ; Set ebp to NULL
+mov ebp, 0x9FC00 ; Set stack again
+mov esp, ebp
+xor ebp, ebp ; Set ebp to NULL for stack traces
 call kmain
 jmp $ ; Again, we should never get here
